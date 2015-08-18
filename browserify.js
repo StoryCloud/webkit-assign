e82021643bc2e7bccf530bd7a6c6d6affb47b979
Peace Chen <pc_5@yahoo.com>
2015-08-18 16:39:36 -0500

    Formatting changes as requested by ccamarat

--- browserify.js
+++ browserify.js
@@ -1,7 +1,14 @@
+// Browserify wrapper for webkit-assign that returns a stream.
+//
+// A wrapper is necessary because webkitAssign(filename) in index.js checks 
+// whether the first parameter is a string. Since Browserify passes the filename 
+// as the first param, webkitAssign calls transformCode() which returns a string 
+// instead of a stream expected by Browserify.
+
 'use strict';
 
 var webkitAssign = require('./index');
 
-module.exports = function (file) {
-	return webkitAssign();
+module.exports = function () {
+    return webkitAssign();
 };
