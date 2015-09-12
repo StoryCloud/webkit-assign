f23b38fe7858e759d716f3e00510758fc99fe8d6
Jackson Hamilton <jackson@jacksonrayhamilton.com>
2015-09-12 12:24:47 -0700

    Remove trailing whitespace.

--- browserify.js
+++ browserify.js
@@ -1,8 +1,8 @@
 // Browserify wrapper for webkit-assign that returns a stream.
 //
-// A wrapper is necessary because webkitAssign(filename) in index.js checks 
-// whether the first parameter is a string. Since Browserify passes the filename 
-// as the first param, webkitAssign calls transformCode() which returns a string 
+// A wrapper is necessary because webkitAssign(filename) in index.js checks
+// whether the first parameter is a string. Since Browserify passes the filename
+// as the first param, webkitAssign calls transformCode() which returns a string
 // instead of a stream expected by Browserify.
 
 'use strict';
