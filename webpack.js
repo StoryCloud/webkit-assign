560bb59dd6762471bd5d11df83b1530598d78883
Rowan Crawford <wombleton@gmail.com>
2015-09-22 11:58:57 +1200

    Linting in Rome.

--- webpack.js
+++ webpack.js
@@ -1,7 +1,9 @@
+'use strict';
+
 // From: https://github.com/angular/angular.js/issues/9128#issuecomment-140314563
 var webkitAssign = require('./index');
 
 module.exports = function (source) {
-  this.cacheable();
-  return webkitAssign(source);
+    this.cacheable();
+    return webkitAssign(source);
 };
